337be3781fbffb40cefcdeb7218a0950bb97af8f
felixyangyicheng <admin-fr06ab@ermo-group.tosb>
2024-04-02 14:45:45 +0200

    docker compose

--- OneTransfert.srv/Program.cs
+++ OneTransfert.srv/Program.cs
@@ -17,17 +17,20 @@ namespace OneTransfert.srv
                     //listenOptions.UseHttps();
                 });
             });
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-            builder.Services.AddSignalR();
             builder.Services.AddCors(options => {
                 options.AddPolicy("AllowAll",
-                    b => b.AllowAnyMethod()
+                    b => b
+                    .AllowAnyMethod()
                     .AllowAnyHeader()
-                     .SetIsOriginAllowed(origin => true) // allow any origin
+                    .SetIsOriginAllowed(origin => true) // allow any origin
+                    .AllowCredentials()
                     .WithExposedHeaders("X-Pagination")
                        );
             });
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+            builder.Services.AddSignalR();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -41,31 +44,17 @@ namespace OneTransfert.srv
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseCors("AllowAll");
 
             app.UseAuthorization();
-            app.MapHub<FileTransferHub>("/file-transfer-hub");
+            //app.MapHub<FileTransferHub>("/file-transfer-hub");
 
-            var summaries = new[]
+            app.UseEndpoints(endpoints =>
             {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+       
+                endpoints.MapHub<FileTransferHub>("/file-transfer-hub");
+            });
 
             app.Run();
         }
